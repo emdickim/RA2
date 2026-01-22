3b70c0896ec838051c7cd562225062ba9367d9dc
emdickim <kimlazaropardo@gmail.com>
2026-01-22 23:32:30 +0100

    Exercici acabat!!!!

--- 03-Sleep_i_cohet/Coet.java
+++ 03-Sleep_i_cohet/Coet.java
@@ -1,5 +1,56 @@
 // AIXO ES UN COHET MOLT XULO!!
+import java.util.Scanner;
 
 public class Coet {
+
+    // Array de 4 motors del coet
+    private static Motor[] motores = new Motor[4];
+
+    // Crea i arrenca els 4 motors
+    private static void arrancarMotors(int potenciaInicial) {
+        for (int i = 0; i < motores.length; i++) {
+            motores[i] = new Motor("Motor " + i);
+            motores[i].setPotenciaObjectiu(potenciaInicial);
+            motores[i].start();
+        }
+    }
+
+    // Mètode que comprova la potència i la passa als 4 motors
+    private static void passaAPotencia(int p) {
+        if (p < 0 || p > 10) {
+            System.out.println("Numero incorrecte, torna-ho a provar");
+            return;
+        }
+
+        System.out.println("Passant a potencia " + p);
+        for (Motor m : motores) {
+            m.setPotenciaObjectiu(p);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introdueix la potencia del cohet: (0-10, escriu 0 per sortir)");
+
+        
+        int numero = sc.nextInt();
+        // Crear i arrencar els motors UNA VEGADA
+        arrancarMotors(numero);
+
+        
+        while (true) {
+            numero = sc.nextInt();
+            if (numero == 0) {
+                // Posem potència 0 i sortim
+                passaAPotencia(0);
+                break;
+            }
+
+            passaAPotencia(numero);
+        }
+        
+        sc.close();
+    }
+
     
 }
--- 03-Sleep_i_cohet/Motor.java
+++ 03-Sleep_i_cohet/Motor.java
@@ -1,15 +1,57 @@
-// AIXO ES UN MOTOR!
+// AIXO ES UN MOTOR!!!!
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Motor extends Thread {
     private int potenciaObjectiu = 0;
     private int potenciaActual = 0;
+    private boolean enObjectiu = false;
 
-    public Motor(int potenciaObjectiu) {
-        this.potenciaActual = potenciaActual;
+    public Motor(String nom) {
+        super(nom);
+    }
+
+    // NO fa falta sumar, la lògica del run ja incrementa/decrementa
+    public void setPotencia(int p) {
+        this.potenciaActual += p;
+    }
+
+    public void setPotenciaObjectiu(int potenciaObjectiu) {
         this.potenciaObjectiu = potenciaObjectiu;
     }
 
-    public void setPotencia(int p) {
-        this.potenciaActual += p;
+    @Override
+    public void run() {
+
+        while (true) {
+            int numero = ThreadLocalRandom.current().nextInt(1, 3);
+
+            if (potenciaActual < potenciaObjectiu) {
+                enObjectiu = false;
+                System.out.println(getName() + ": Incre. Objectiu: " + potenciaObjectiu + " Actual: " + potenciaActual);
+                potenciaActual++;
+            } else if (potenciaActual > potenciaObjectiu) {
+                
+                System.out.println(getName() + ": Decre. Objectiu: " + potenciaObjectiu + " Actual: " + potenciaActual);
+                enObjectiu = false;
+                potenciaActual--;
+            } else {
+                if (!enObjectiu) {
+                    System.out.println(getName() + ": FerRes Objectiu: " + potenciaObjectiu + " Actual: " + potenciaActual);
+                    enObjectiu = true;
+                    // Si hem arribat a potència 0 i l'objectiu també és 0, sortim del bucle
+                    if (potenciaObjectiu == 0 && potenciaActual == 0) {
+                        break;
+                    }
+                 }
+            }
+
+            
+
+            try {
+                Thread.sleep(numero * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
