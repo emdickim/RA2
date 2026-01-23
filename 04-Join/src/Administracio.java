4d76011b085995ff8e20ab766ce640f147a5ff16
emdickim <kimlazaropardo@gmail.com>
2026-01-23 19:15:29 +0100

    Acabat a classe :D

--- 04-Join/src/Administracio.java
+++ 04-Join/src/Administracio.java
@@ -5,17 +5,38 @@ public class Administracio extends Thread {
     private final int num_poblacio_activa = 50;
     private static final Treballador[] poblacio_activa = new Treballador[50];
 
-
-    public static void main(String[] args) {
-        constructor();
-    }
-
-
-    private static void constructor() {
-        
+    public Administracio() {            // <-- AIXÒ és el constructor
         for (int i = 0; i < poblacio_activa.length; i++) {
             poblacio_activa[i] = new Treballador(25000, 20, 65);
-            poblacio_activa[i].setName("Ciutadà-" + 1);
+            poblacio_activa[i].setName("Ciutadà-" + i);
         }
     }
+
+
+    public static void main(String[] args) {
+        Administracio admin = new Administracio();
+
+        for (int i = 0; i < poblacio_activa.length; i++) {
+            poblacio_activa[i].start();              
+        }
+
+        
+        for (int i = 0; i < poblacio_activa.length; i++) {
+           try {
+                poblacio_activa[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.printf("%s -> edat: %d / total: %.2f%n",
+            admin.poblacio_activa[i].getName(),
+            admin.poblacio_activa[i].getEdat(),
+            admin.poblacio_activa[i].getCobrat());
+            
+        }
+        
+    }
+
+
+    
 }
--- 04-Join/src/Treballador.java
+++ 04-Join/src/Treballador.java
@@ -5,14 +5,14 @@ import java.util.Random;
 public class Treballador extends Thread {
     
     //private static final String nom;
-    private static final int SOU_ANUAL_BRUT;
-    private static final int EDAT_INICI_TREBALL;
-    private static final int EDAT_FI_TREBALL;
-    private static int edatActual;
-    private static float cobrat = 0.0f;
-    private static Random rnd = new Random();
+    private float SOU_ANUAL_BRUT;
+    private int EDAT_INICI_TREBALL;
+    private int EDAT_FI_TREBALL;
+    private int edatActual;
+    private float cobrat = 0.0f;
+    private Random rnd = new Random();
 
-    public Treballador(int souAnualBrut, int edatIniciTreball, int edatFiTreball) {
+    public Treballador(float souAnualBrut, int edatIniciTreball, int edatFiTreball) {
         this.SOU_ANUAL_BRUT = souAnualBrut;
         this.EDAT_INICI_TREBALL = edatIniciTreball;
         this.EDAT_FI_TREBALL = edatFiTreball;
@@ -20,35 +20,40 @@ public class Treballador extends Thread {
     }
     
     public void cobra() {
-        this.cobrat += SOU_ANUAL_BRUT / 12f; 
+        this.cobrat += (SOU_ANUAL_BRUT / 12.0f); 
     }
 
     public void pagaImpostos() {
-        this.cobrat -= (SOU_ANUAL_BRUT / 12f) * 0.24f; 
+        this.cobrat -= (SOU_ANUAL_BRUT / 12.0f) * 0.24f; 
     }
 
-    public static float getCobrat() {
+    public float getCobrat() {
         return cobrat;
     }
 
-    public static int getEdatActual() {
+    public int getEdat() {
         return edatActual;
     }
 
     @Override
     public void run() {
         while (true) {
-            int tempsSleep = rnd.nextInt(1000);
             edatActual++;
-            if (edatActual >= 20) {
-                cobra();
-                pagaImpostos();
+            if (edatActual >= 20 && edatActual < 65) {
+                for (int i = 0; i < 12; i++) {
+                    cobra();
+                    pagaImpostos();
+                }
+            } 
+            
+            if (edatActual == 65) {
+                break;
             }
 
-            if (edatActual == 65) break;
+            
             
             try {
-                Thread.sleep(tempsSleep);
+                Thread.sleep(rnd.nextInt(10));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
