5ecddc49133558e2593153377961c210be68a216
emdickim <kimlazaropardo@gmail.com>
2026-02-06 17:27:40 +0100

    07-Filòsofs final de classe

---
+++ 07-Filòsofs.java/src/Forquilla.java
@@ -0,0 +1,27 @@
+public class Forquilla {
+    int numeroForquilles;
+    private boolean enUs;
+
+    public Forquilla(int numeroForquilles) {
+        this.numeroForquilles = numeroForquilles;
+    }
+
+        public int getNumeroForquilles() {
+        return numeroForquilles;
+    }
+
+    public void setNumeroForquilles(int numeroForquilles) {
+        this.numeroForquilles = numeroForquilles;
+    }
+
+    public boolean isEnUs() {
+        return enUs;
+    }
+
+    public void setEnUs(boolean enUs) {
+        this.enUs = enUs;
+    }
+
+
+    
+}
---
+++ 07-Filòsofs.java/src/Taula.java
@@ -0,0 +1,5 @@
+public class Taula {
+    
+
+    int numeroFilosofs;
+}
